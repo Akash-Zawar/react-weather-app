bf09d891afe02ae4f44fc6df0468625fe4aa7f50
Akash-Zawar <zawarakash1707@gmail.com>
2023-11-29 18:05:59 +0530

    weather app project

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import Search from "./Components/Search/search";
+import CurrentWeather from "./Components/Current_Weather/currentWeather";
+import { weatherKey, weatherUrl } from "./Components/Search/api";
+import { useState } from "react";
+import Forecast from "./Components/forecast/forecast";
+
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${weatherUrl}/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=metric`
+    );
+
+    const forecastFetch = fetch(
+      `${weatherUrl}/forecast?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  console.log({ currentWeather });
+  console.log({ forecast });
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/forecast/forecast.js
@@ -0,0 +1,84 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+import "./forecast.css";
+
+const weekDays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const Forecast = ({ data }) => {
+  const dayInWeek = new Date().getDay();
+  const forecastDays = weekDays
+    .slice(dayInWeek, weekDays.length)
+    .concat(weekDays.slice(0, dayInWeek));
+
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => {
+          return (
+            <AccordionItem key={idx}>
+              <AccordionItemHeading>
+                <AccordionItemButton>
+                  <div className="daily-item">
+                    <img
+                      alt="weather"
+                      className="icon-small"
+                      src={`icons/${item.weather[0].icon}.png`}
+                    />
+                    <label className="day">{item.dt_txt.split(" ")[0]}</label>
+                    <label className="description">
+                      {item.weather[0].description}
+                    </label>
+                    <label className="min-max">
+                      {Math.round(item.main.temp_min)}°C
+                    </label>
+                  </div>
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+                <div className="daily-details-grid">
+                  <div className="daily-details-grid-item">
+                    <label>Pressure:</label>
+                    <label>{item.main.pressure} hPa</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Humidity:</label>
+                    <label>{item.main.humidity}%</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Clouds:</label>
+                    <label>{item.clouds.all}%</label>
+                  </div>
+                  <div className="daily-details-grid-item">
+                    <label>Wind speed:</label>
+                    <label>{item.wind.speed} m/s</label>
+                  </div>
+
+                  <div className="daily-details-grid-item">
+                    <label>Feels like:</label>
+                    <label>{item.main.feels_like}°C</label>
+                  </div>
+                </div>
+              </AccordionItemPanel>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
+    </>
+  );
+};
+export default Forecast;
